4dfdeafe8f24ebdb7ce19539973891f57a23fb38
Evandro Schlumpf <sch.evandro@gmail.com>
2019-05-07 16:30:16 +0300

    Atualização .gitignore

---
+++ admin/documentos/comprovantes_guia/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
+	<title></title>
+	<meta name="generator" content="LibreOffice 5.0.3.2 (Linux)"/>
+	<meta name="created" content="00:00:00"/>
+	<meta name="changedby" content="Evandro Schlumpf"/>
+	<meta name="changed" content="2016-10-14T13:30:53.186075591"/>
+</head>
+<body lang="pt-BR" dir="ltr">
+<p>&lt;?php header('Location:../../../index.php'); ?&gt; 
+</p>
+</body>
+</html>
